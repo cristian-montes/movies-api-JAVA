a0677ca9ef4b02f9d9870cc158841ef6e26749b2
Cristian Montes <cmontesmujica@gmail.com>
2023-05-03 21:16:19 -0600

    added logic to post reviews for a movie by id

--- src/main/java/dev/lostacodecarneasada/Movies/ReviewService.java
+++ src/main/java/dev/lostacodecarneasada/Movies/ReviewService.java
@@ -3,6 +3,7 @@ package dev.lostacodecarneasada.Movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +16,14 @@ public class ReviewService {
 
     public Review createReviewBy(String reviewBody, String imdbId){
             Review review = new Review(reviewBody);
-            reviewRepository.insert(review);
+            reviewRepository.insert(new Review(reviewBody));
 
             mongoTemplate.update(Movie.class)
                     .matching(Criteria.where("imdbId").is(imdbId))
+                    .apply(new Update().push("reviewIds").value(review))
+                    .first();
 
+            return review;
 
     };
 }
